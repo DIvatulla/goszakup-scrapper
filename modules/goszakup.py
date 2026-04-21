a04abb3ac7a87f280cc1b32a1c2e86485c9f1ec1
DIvatulla <ivatulladaniel0@gmail.com>
2026-04-21 14:50:50 +0500

    addition of unified session is needed

--- modules/goszakup.py
+++ modules/goszakup.py
@@ -4,8 +4,11 @@ from http_module import https
 from http_module import http_request
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+from urllib.parse import urlsplit
 import time
 import re
+import requests
+
 
 class goszakup_filters:
 	def __init__(self, name: str="", customer: str="", spec: str="",\
@@ -53,53 +56,80 @@ class goszakup:
 
 	def get_request(self) -> str:
 		req = http_request(host="https://goszakup.gov.kz", path=(self.filters.urlify()), headers={"Host": "goszakup.gov.kz",
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36",
 		"Content-Type": "*/*"})	
 		return https.get(req).text
 
 	def __count_pages(self) -> int:
 		self.count = 0
-		st = 0
-		html = self.get_request()
+		html_doc = self.get_request()
+		soup = BeautifulSoup(html_doc, features="lxml")
+		text = ((soup.find_all("small")[0]).find("strong")).get_text(strip=True)
+		match = re.search(f"{r"из "}(.*?){r" записей"}", text)
+		self.count = int(match.group(1))
 		
-		for line in html.splitlines():
-			match st:
-				case 0:
-					if "<small class=\"text-muted\">" in line:
-						st += 1
-						continue
-				case 1:
-					print(line)
-					match = re.search(f"{r"из "}(.*?){r" записей"}", line)
-					self.count = int(match.group(1))
-					break
-
 class excel:
 	@classmethod
-	def __parse_html(cls, html_doc: str) -> list:
+	def parse_cell(cls, c) -> dict:
+		result = {"content": None, "url": None}
+
+		url_tag = c.find('a')
+		if url_tag:
+			result["url"] = url_tag.get('href')
+
+		result["content"] = c.get_text(strip=True)
+
+		return result
+
+	@classmethod
+	def parse_html(cls, html_doc: str) -> list:
 		table = []
 		soup = BeautifulSoup(html_doc, features="lxml")
 		
-		for tr in (soup.find_all('tbody')[1]).find_all('tr'):
+		for tr in ((soup.find_all('tbody')[1]).find_all('tr')):
 			cells = tr.find_all('td')
-			row = []
-			
-			for c in cells:
-				buf = {}
-				buf["content"] = c.get_text(strip=True)
-				buf["url"] = None
-
-				link_tag = c.find('a')
-				if link_tag:
-					url = link_tag.get('href')
-					buf["url"] = url
-
-				row.append(buf)
-			
-			table.append(row)
+			(list(map(
+				lambda x: table.append(x),
+				(list(map(lambda cell: cls.parse_cell(cell), cells)))
+			)))
+			#table.append(list(map(lambda cell: cls.parse_cell(cell), cells)))
+			print(table[-1])
 
 		return table
 
+	@classmethod
+	def make_table(cls, gz: goszakup, filename: str):
+		for i in range(gz.filters.count_record, gz.count+gz.filters.count_record, gz.filters.count_record):
+			gz.filters.page += 1
+			for row in cls.parse_html(gz.get_request()):
+				print(row)
+				
+				if (row["content"] == '') and (row["url"] != None):
+					time.sleep(5)
+					res = requests.get(row["url"], {
+						"Host": "goszakup.gov.kz",
+						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0",
+						"Content-Type": "*/*"}).text
+					soup = BeautifulSoup(res, features="lxml")
+					row["content"] = (soup.find_all('title')[0]).get_text(strip=True)
+				
+				print(row)	
+				time.sleep(10)
+
+
+				"""
+				if row["content"] == row["url"]:
+					res = requests.get(row["url"]).text
+					print(res)
+					exit(1)
+					soup = BeautifulSoup(res, features="lxml")
+					org_name = (soup.find_all('title')[0]).get_text(strip=True)
+					result["content"] = re.split(r" ::", org_name)[0]
+
+				print(row)
+				time.sleep(10)
+				"""
+"""
 	@classmethod
 	def make_table(cls, gz: goszakup, filename: str):
 		print(gz.count)
@@ -121,6 +151,7 @@ class excel:
 				
 				for col_idx, cell_dict in enumerate(row, start=1):
 					if cell_dict["url"]:
+						print(cell_dict)
 						cell_obj = ws.cell(row=ws.max_row, column=col_idx)
 						cell_obj.hyperlink = cell_dict["url"]
 						cell_obj.style = "Hyperlink"
@@ -128,5 +159,6 @@ class excel:
 			time.sleep(5)
 		
 		wb.save(filename)
+"""
 
 				
